148963301d9b6a39eee64c04597164ceabe23ca1
Mika Repo <mikeorap@gmail.com>
2023-04-28 17:46:39 +0300

    Osa 3 tehtäviä 21 asti

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
-
-//tehty 3.3 viimeksi...
+app.use(express.static('build'))
 
 app.use(express.json())
-
-app.use(express.static('build'))
 
 app.use(cors())
 
@@ -42,26 +41,31 @@ let persons = [
     }
 ]
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(nimi => nimi.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-    const luku = persons.length
-    const date = new Date()
-    res.send(
-        `<div><p>Phonebook has info for ${luku} people</p><p>${date}</p></div>`
-    )
+app.get('/info', (req, res, next) => {
+    Person.countDocuments()
+      .then(count => {
+        console.log(count)
+        const date = new Date()
+        res.send(
+          `<div><p>Phonebook has info for ${count} people</p><p>${date}</p></div>`
+        )
+      })
+      .catch(error => next(error))
 })
 
 const generateId = () => {
   return Math.floor(Math.random() * 100)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -84,26 +88,26 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(nimi => nimi.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
   })
 
 app.get('/', (req, res) => {
@@ -111,10 +115,44 @@ app.get('/', (req, res) => {
   })
   
   app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(people => {
+      people.forEach(person => {
+        console.log(person)
+      })
+      res.json(people)
+    })
   })
+
+  app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {runValidators: true, context: 'query'})
+      .then(updatedPerson => {
+        res.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
   
-  const PORT = process.env.PORT || 3001
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({error: error.message})
+    }
+  
+    next(error)
+  }
+
+  app.use(errorHandler)
+  
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
